1c4dbcc4b7ed48ee7e560108d9a551d2b2b38de4
carlos <carlosgimenoortiz@gmail.com>
2023-12-28 12:57:48 +0100

    initial commit

---
+++ portfolio-dev/Portfolio/src/pages/newFile.tsx
@@ -0,0 +1,11 @@
+import Layout from '../layouts/Layout.astro';
+
+<Fragment>
+<Layout title="Welcome to Astro.">
+<main>
+<h1> Hey, soy Carlos <Badge>Available6</Badge></h1>
+</main>
+</Layout>
+
+
+</Fragment>;
